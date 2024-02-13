cbc57a57686f2342eed52bdbe90c8fbfd578e4ce
Mustafa Oguzhan Savas <MustafaOguzhan.Savas@bs.nttdata.com>
2024-02-13 14:40:27 +0300

    appsettings change

--- HangFire.WEB/Services/EmailSender.cs
+++ HangFire.WEB/Services/EmailSender.cs
@@ -20,9 +20,9 @@ namespace HangFire.WEB.Services
 
             var apiKey = _configuration.GetSection("APIs")["SendGridApi"];
             var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("swouz@hotmail.com", "Example User");
+            var from = new EmailAddress("test@hotmail.com", "Example User");
             var subject = "www.mysite.com bilgilendirme";
-            var to = new EmailAddress("oguzsavas.43@gmail.com", "Example User");
+            var to = new EmailAddress("testemail@gmail.com", "Example User");
             //var plainTextContent = "and easy to do anywhere, even with C#";
             var htmlContent = $"<strong>{message}</strong>";
             var msg = MailHelper.CreateSingleEmail(from, to, subject, null, htmlContent);
